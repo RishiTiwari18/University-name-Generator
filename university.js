538c0c49e33ae4df79a949fd2d92565dfa1ee868
RishiTiwari18 <rishiitiwarii18@gmail.com>
2026-02-10 17:53:25 +0530

    Update university.js

--- university.js
+++ university.js
@@ -1,31 +1,34 @@
-let url="https://universities.hipolabs.com/search?name=";
-let btn=document.querySelector("button");
-btn.addEventListener("click",async ()=>{
-    let country =document.querySelector("input").value;
+let url = "https://universities.hipolabs.com/search?name=";
+
+let btn = document.querySelector("#b");
+
+btn.addEventListener("click", async () => {
+    let country = document.querySelector("input").value;
     console.log(country);
+
     let collegeArr = await getcolleges(country);
     console.log(collegeArr);
+
     show(collegeArr);
 });
-function show(collegeArr){
-    let list=document.querySelector("#list");
-    list.innerText="";
-    for( let col of collegeArr){
-        console.log(col.name);
-    let li = document.createElement("li");
-    li.innerText=col.name;
-    list.appendChild(li);
-}
+
+function show(collegeArr) {
+    let list = document.querySelector("#list");
+    list.innerText = "";
+
+    for (let col of collegeArr) {
+        let li = document.createElement("li");
+        li.innerText = col.name;
+        list.appendChild(li);
+    }
 }
 
-async function getcolleges(country){
-    try{
-         let res = await axios.get(url + encodeURIComponent(country));
+async function getcolleges(country) {
+    try {
+        let res = await axios.get(url + encodeURIComponent(country));
         return res.data;
-
-    }catch(e){
-        console.log("error :",e);
+    } catch (e) {
+        console.log("error:", e);
         return [];
-
     }
 }
